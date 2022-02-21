50b5b9eff83aa6924a10f99d7b4856538f485fd7
Youssef12T1B1 <yousseattabi1998@gmail.com>
2022-02-21 00:47:39 -0800

    display products

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const router = express.Router()
-
-
+const axios = require('axios')
 
 router.get('/products', (req,res)=>{
-    res.render('products')
-})
-
+    var categories 
+    axios.get('http://localhost:3000/api/products/')
+    .then(function(productData){
+      
+    axios.get('http://localhost:3000/api/categories')
+    .then(function(response){
+         categories  =response.data
+         
+         res.render('products', {products:productData.data,categories:categories})
+      })
+       
+  
+    })
+    .catch(err=>{
+        res.send(err)
+    })
+  })
+  
 
 module.exports = router
